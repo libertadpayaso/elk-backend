ddbf17e716213b994d2f603449a249990b0cb3e2
Pablo Gimenez <gimenezpgabriel@gmail.com>
2024-12-02 08:44:31 -0300

    nuevo estado de pedido

--- app/Pedido.php
+++ app/Pedido.php
@@ -10,29 +10,31 @@ class Pedido extends Model
     use SoftDeletes;
 
     const ESTADOS_PEDIDO = [
-        0 => 'Sin procesar', 
-        1 => 'Entregado',  
-        2 => 'Contactado', 
-        3 => 'Cancelado',
-        4 => 'Completo y pagado',
-        5 => 'inCompleto',
-        6 => 'Pagado SIN ARMAR',
-        7 => 'Retira y Paga en local',
-        8 => 'Pagado y Retira en local',
-        9 => 'Completo F/PAGO'
+        0  => 'Sin procesar',
+        1  => 'Entregado',
+        2  => 'Contactado',
+        3  => 'Cancelado',
+        4  => 'Completo y pagado',
+        5  => 'inCompleto',
+        6  => 'Pagado SIN ARMAR',
+        7  => 'Retira y Paga en local',
+        8  => 'Pagado y Retira en local',
+        9  => 'Completo F/PAGO',
+        10 => 'Enviado al local'
     ];
 
     const COLORES_ESTADO = [
-        0 => '#fefe8b', 
-        1 => '#b4ffb4',  
-        2 => '#d3cfff', 
-        3 => '#fbb1b1',
-        4 => '#7fffd4',
-        5 => '#fdcb6e',
-        6 => '#dfe6e9',
-        7 => '#33EEFF',
-        8 => '#d8ffad',
-        9 => '#ff9ee0'
+        0  => '#fefe8b',
+        1  => '#b4ffb4',
+        2  => '#d3cfff',
+        3  => '#FF0000',
+        4  => '#7fffd4',
+        5  => '#fdcb6e',
+        6  => '#dfe6e9',
+        7  => '#33EEFF',
+        8  => '#d8ffad',
+        9  => '#ff9ee0',
+        10 => '#4f8ee8'
     ];
     
     protected $fillable = [
